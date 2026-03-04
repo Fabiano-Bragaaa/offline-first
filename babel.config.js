01756f334907cb783745a9e16c59eae70ab94c9b
fabiano-bragaaa <fabianobragadev@gmail.com>
2026-03-04 15:41:14 -0300

    feat: add module resolver configuration and update TypeScript paths

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,26 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        alias: {
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@routes': './src/routes',
+          '@screens': './src/screens',
+          '@utils': './src/utils',
+          '@theme': './src/theme',
+          '@hooks': './src/hooks',
+          '@translate': './src/translate',
+          '@domain': './src/domain',
+          '@services': './src/services',
+          '@api': './src/api',
+          '@infra': './src/infra',
+          '@errors': './src/errors',
+        },
+      },
+    ],
+  ],
 };
