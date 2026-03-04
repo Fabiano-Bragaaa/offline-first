c9a421b60eca675f114038ef7e228b6728ab6cb4
fabiano-bragaaa <fabianobragadev@gmail.com>
2026-03-04 16:04:20 -0300

    refactor: implement singleton pattern for Realm instance and clean up ServiceOrder type definition

--- src/infra/persistence/realm/realm.ts
+++ src/infra/persistence/realm/realm.ts
@@ -1,7 +1,15 @@
 import Realm from 'realm';
 import { ServiceOrderSchema } from './schemas/service-order-schema';
 
-export const realm = new Realm({
-  schema: [ServiceOrderSchema],
-  schemaVersion: 1,
-});
+let realmInstance: Realm | null = null;
+
+export function getRealm() {
+  if (!realmInstance) {
+    realmInstance = new Realm({
+      schema: [ServiceOrderSchema],
+      schemaVersion: 1,
+    });
+  }
+
+  return realmInstance;
+}
--- src/infra/persistence/realm/schemas/service-order-schema.ts
+++ src/infra/persistence/realm/schemas/service-order-schema.ts
@@ -1,18 +1,5 @@
 import { ObjectSchema } from 'realm';
 
-export type ServiceOrder = {
-  id: string;
-  title: string;
-  description: string;
-  status: 'Pending' | 'In Progress' | 'Completed';
-  assignedTo: string;
-  createdAt: Date;
-  updatedAt: Date;
-  deletedAt?: Date | null;
-  completed: boolean;
-  deleted: boolean;
-};
-
 export const ServiceOrderSchema: ObjectSchema = {
   name: 'ServiceOrder',
   primaryKey: 'id',
