5de7f9639b1b1cf3107e6bf6512ea6190177c1ba
fabiano-bragaaa <fabianobragadev@gmail.com>
2026-03-05 18:44:54 -0300

    feat: enhance App component with SafeAreaProvider and StatusBar for improved layout and appearance

--- App.tsx
+++ App.tsx
@@ -2,14 +2,19 @@ import './global.css';
 import React from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Routes } from '@routes';
+import { StatusBar } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Routes />
-    </QueryClientProvider>
+    <SafeAreaProvider>
+      <QueryClientProvider client={queryClient}>
+        <StatusBar translucent barStyle="dark-content" />
+        <Routes />
+      </QueryClientProvider>
+    </SafeAreaProvider>
   );
 }
 
