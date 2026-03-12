9931ed56378b589960bf547f2f93d05628dd48c5
fabiano-bragaaa <fabianobragadev@gmail.com>
2026-03-12 11:45:28 -0300

    feat: update Realm schema to version 2 and implement migration logic for ServiceOrder to reset pendingAction and syncError fields

--- src/infra/persistence/realm/realm.ts
+++ src/infra/persistence/realm/realm.ts
@@ -1,5 +1,6 @@
 import Realm from 'realm';
 import { ServiceOrderSchema } from './schemas/service-order-schema';
+import { OrderRaw } from '@domain';
 
 let realmInstance: Realm | null = null;
 
@@ -7,7 +8,16 @@ export function getRealm() {
   if (!realmInstance) {
     realmInstance = new Realm({
       schema: [ServiceOrderSchema],
-      schemaVersion: 1,
+      schemaVersion: 2,
+      onMigration: (oldRealm, newRealm) => {
+        if (oldRealm.schemaVersion < 2) {
+          const orders = newRealm.objects<OrderRaw>('ServiceOrder');
+          orders.forEach((order) => {
+            order.pendingAction = null;
+            order.syncError = null;
+          });
+        }
+      },
     });
   }
 
